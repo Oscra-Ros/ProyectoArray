491fce1e6576da2fbdab33af09406baaf5b959cd
Oscra <Oscra@gmail.com>
2021-01-25 10:06:06 +0100

    2 SNAPSHOT Main1.java array double

--- src/misrc/Main1.java
+++ src/misrc/Main1.java
@@ -9,6 +9,11 @@ public class Main1 {
         for(int i=0; i<nombre.length; i++){
             System.out.println(nombre[i]);
         }
+        //Declaración array tipo double
+        double[] estatura = {1.72, 1.75, 1.65};
+        for(int i=0; i<estatura.length; i++){
+            System.out.println(estatura[i]);
+        }
     }
     
 }
