7da450af177dd514052c21046f61b64703fced28
Oscra <Oscra@gmail.com>
2021-01-25 10:00:36 +0100

    1 SNAPSHOT Main1.java

---
+++ src/misrc/Main1.java
@@ -0,0 +1,14 @@
+package misrc;
+
+
+public class Main1 {
+
+    
+    public static void main(String[] args) {
+        String[] nombre = {"Luis", "Miguel", "Carlos"};
+        for(int i=0; i<nombre.length; i++){
+            System.out.println(nombre[i]);
+        }
+    }
+    
+}
